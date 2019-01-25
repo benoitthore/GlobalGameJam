9123d805b750a947ae7ab080d1896bdddbebf358
Rafael Keramidas <rafael@keramid.as>
2019-01-25 22:18:00 +0000

    Script de base région

---
+++ Assets/Region.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Region : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(worldMousePosition);
+    }
+}
