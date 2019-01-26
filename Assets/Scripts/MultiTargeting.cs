14579759bb394cce77bd2ec9107f42bb5807fa56
Benoit Thore <thorebenoit@gmail.com>
2019-01-26 15:46:33 +0000

    Mutli targeting update

--- Assets/Scripts/MultiTargeting.cs
+++ Assets/Scripts/MultiTargeting.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class MultiTargeting : Targeting
 {
     private TargetingPredefined targetingPredefined;
     private TargetingWithRadius targetingWithRadius;
 
-    private void Start()
+    // TODO Add 
+    
+
+    void Awake()
     {
         targetingPredefined = gameObject.AddComponent<TargetingPredefined>();
         targetingWithRadius = gameObject.AddComponent<TargetingWithRadius>();
-
     }
 
     public override GameObject getTarget()
