f17007d4552466ca483ab1cee0e23b2952a04f68
Rafael Keramidas <rafael@keramid.as>
2019-01-26 21:18:06 +0000

    Added menus for credits and options

---
+++ Assets/Credits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            GoToMenu();
+        }
+    }
+    
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
