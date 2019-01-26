208e19637eb397e86168f1938a48cfaf8a14e7c3
Benoit Thore <thorebenoit@gmail.com>
2019-01-26 00:50:04 +0000

    Adding turrent following:

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject selectedTurret;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var mousePosition = Input.mousePosition;
+            var mousePositionWorld = Camera.main.ScreenToWorldPoint(new Vector2(mousePosition.x, mousePosition.y));
+            mousePositionWorld.z = transform.position.z;
+            placeTurretAt(mousePositionWorld);
+        }
+    }
+
+    private void placeTurretAt(Vector3 position)
+    {
+        Debug.Log(position);
+        Instantiate(selectedTurret, position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inCircleList = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (var obj in inCircleList)
+        {
+            Debug.Log("Overlapping" + obj.name);
+            var target = obj.gameObject.GetComponent<TurretTarget>();
+
+            if (target)
+            {
+                Vector2 from = transform.position;
+                Vector2 to = target.transform.position;
+                var direction = new Vector2(from.x - to.x, from.y - to.y);
+                transform.transform.up = -direction;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+//    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
